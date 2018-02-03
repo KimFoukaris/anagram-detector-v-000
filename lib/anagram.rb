cc1fe6ff6e73e450974506566c06bfcba441d5d7
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 14:02:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,17 +8,18 @@ class Anagram
 
   def match(word_array)
     matches = []
-    word_test = word.split(//)
-    word_test = word_test.sort!
-    word_array.each do |w|
-      letters = w.split(//)
-      letters = letters.sort!
-      if letters = word_test
-        matches << w
-      end
-    end
-    word_array = matches
-    word_array
+    matches
+    #word_test = word.split(//)
+    #word_test = word_test.sort!
+    #word_array.each do |w|
+      #letters = w.split(//)
+      #letters = letters.sort!
+      #if letters = word_test
+        #matches << w
+      #end
+    #end
+    #word_array = matches
+    #word_array
   end
 
 end
