0f6b90db68b9e984a29e6f9933f5e4db5e910eb5
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 14:02:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,6 @@ class Anagram
 
   def match(word_array)
     matches = []
-    matches
     #word_test = word.split(//)
     #word_test = word_test.sort!
     #word_array.each do |w|
@@ -18,8 +17,7 @@ class Anagram
         #matches << w
       #end
     #end
-    #word_array = matches
-    #word_array
+    matches
   end
 
 end
