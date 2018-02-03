05628e200aad05fa256eaf9ca5e9de50749b5f2f
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 14:05:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,15 @@ class Anagram
 
   def match(word_array)
     matches = []
-    #word_test = word.split(//)
-    #word_test = word_test.sort!
-    #word_array.each do |w|
-      #letters = w.split(//)
-      #letters = letters.sort!
-      #if letters = word_test
-        #matches << w
-      #end
-    #end
+    word_test = word.split(//)
+    word_test = word_test.sort!
+    word_array.each do |w|
+      letters = w.split(//)
+      letters = letters.sort!
+      if letters = word_test
+        matches << w
+      end
+    end
     matches
   end
 
